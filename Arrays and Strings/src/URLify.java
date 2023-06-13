bb016cebff08e4659551fa7e63776d86d5b79b93
sheet-p <sheetal.mec@gmail.com>
2023-06-13 23:44:50 +0100

    fix:formatted output

--- Arrays and Strings/src/URLify.java
+++ Arrays and Strings/src/URLify.java
@@ -1,18 +1,39 @@
 public class URLify {
     public static void main(String[] args) {
-        String str = "Mr John Smith    ";
+        String str = "Mr John Smith      ";
         char[] arr = str.toCharArray();
         //finding true length
         int truelen = findLastCharacter(str) + 1;
-
-        //count spaces
-        int spaces = countSpaces(str, truelen);
 
         replaceSpaces(arr, truelen);
 
         System.out.println(" ");
         for(char ch : arr)
             System.out.print(ch);
+    }
+
+    //assuming string has sufficient space at the end
+    private static void replaceSpaces(char[] str, int truelen) {
+        int spaces = countSpaces(str, truelen);
+
+        //index keeps track of the pointer for the replacement array
+        int index = truelen + (spaces * findLastCharacter("%20"));
+
+        if (truelen < str.length) 
+            str[truelen] = '\0';
+
+        for(int i=truelen-1; i>=0; i--) {
+            if(str[i] == ' ') {
+                str[index-1] = '0';
+                str[index-2] = '2';
+                str[index-3] = '%';
+                index = index-3;
+            }
+            else {
+                str[index-1] = str[i];
+                index--;
+            }
+        }
     }
 
     private static int findLastCharacter(String str) {
@@ -23,30 +44,12 @@ public class URLify {
         return -1;
     }
 
-    private static int countSpaces(String str, int len) {
+    private static int countSpaces(char[] str, int len) {
         int spaces = 0;
         for(int i=0; i<len; i++) {
-            if(str.charAt(i) == ' ')
+            if(str[i] == ' ')
                 spaces++;
         }
         return spaces;
-    }
-
-
-    //assuming string has sufficient space at the end
-    private static void replaceSpaces(char[] str, int len) {
-        int index = str.length-1;
-        for(int i=len-1; i>=0; i--) {
-            if(str[i] == ' ') {
-                str[index] = '0';
-                str[index-1] = '2';
-                str[index-2] = '%';
-                index = index-3;
-            }
-            else {
-                str[index] = str[i];
-                index--;
-            }
-        }
     }
 }
